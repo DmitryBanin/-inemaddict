0a86ac35a302225fb2fdc42eb264ec60360c05ca
Banin <banindm@mail.ru>
2022-05-08 21:44:10 +0300

    удалил лишнее

--- src/view/film-card-view.js
+++ src/view/film-card-view.js
@@ -3,7 +3,6 @@ import dayjs from 'dayjs';
 import { getTimeFromMins } from '../utils.js';
 
 const createFilmCardTemplate = (filmcard) => {
-  console.log(filmcard);
   const { filmInfo, userDetails, comments } = filmcard;
   const { release, title, poster, totalRating, genre, description, runTime } = filmInfo;
   const { date } = release;
